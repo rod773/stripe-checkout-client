880c688e742d39a60852ba0112c485077fe80785
rod773 <rodrigoarcadio1@gmail.com>
2024-05-29 10:46:55 +0200

    update

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -3,7 +3,20 @@ import CardIcon from "images/credit-card.svg";
 import ProductImg from "images/product-image.jpg";
 import { loadStripe } from "@stripe/stripe-js";
 
+let stripePromise;
+
+const getStripe = () => {
+  if (!stripePromise) {
+    stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
+  }
+  return stripePromise;
+};
+
 const Checkout = () => {
+  const item = {
+    price: "price_1PLhPCHui8JNyJ0cnH0WxjiZ",
+  };
+
   return <div>Checkout</div>;
 };
 
