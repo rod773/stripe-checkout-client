82ce119267ba7796edb88491046645b216712692
rod773 <rodrigoarcadio1@gmail.com>
2024-05-29 12:15:20 +0200

    update

--- src/components/Cancel.js
+++ src/components/Cancel.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Cancel = () => {
-  return <div>Cancel</div>;
+  return (
+    <div className="text-2xl">
+      <h1>Pago Cancelado</h1>
+    </div>
+  );
 };
 
 export default Cancel;
